80c53cece2e477d3cabdb873d4bbf189d0ef0c6e
Adam <57396704+Sankti@users.noreply.github.com>
2020-08-09 23:24:15 +0200

    Update 23:24

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import { Provider, createDispatchHook } from 'react-redux';
+import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducer from './store/reducer';
 
